67d77e1786301db430974385bfa5aa9c2225fc9b
Aaryan Dewan <aaryan.dewan@iongroup.com>
2023-02-01 16:43:14 +0530

    feature 3

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -7,6 +7,7 @@ const reducer = (state, action) => {
   console.log("Change 1");
   console.log("Adding feature");
   console.log("adding feature2");
+  console.log("adding feature3");
   switch (action.type) {
     case actionType.SET_USER:
       return {
